1ff4c0c1f788f2194fe6e2cf4d59141eb51ba3b1
dan1114ho <dan1114ho@gmail.com>
2018-07-23 15:00:28 -0400

    Modified order of tour collection api resource

--- app/Mobile/Resources/TourCollection.php
+++ app/Mobile/Resources/TourCollection.php
@@ -37,27 +37,26 @@ class TourCollection extends ResourceCollection
                     'has_prize' => $item->has_prize,
                     'prize_details' => $item->prize_details,
                     'prize_instructions' => $item->prize_instructions,
+                    'trophy_image' => $item->trophyImage ? $item->trophyImage->path : null,
 
                     'start_point' => $item->start_point_id,
                     'start_message' => $item->start_message,
                     'start_video_url' => $item->start_video_url,
+                    'start_image' => $item->startImage ? $item->startImage->path : null,
 
                     'end_point' => $item->end_point_id,
                     'end_message' => $item->end_message,
                     'end_video_url' => $item->end_video_url,
+                    'end_image' => $item->endImage ? $item->endImage->path : null,
 
-                    'media' => [
-                        'image1' => $item->image1 ? new ImageResource($item->image1) : null,
-                        'image2' => $item->image2 ? new ImageResource($item->image2) : null,
-                        'image3' => $item->image3 ? new ImageResource($item->image3) : null,
-                        'main_image' => $item->mainImage ? new ImageResource($item->mainImage) : null,
-                        'start_image' => $item->startImage ? new ImageResource($item->startImage) : null,
-                        'end_image' => $item->endImage ? new ImageResource($item->endImage) : null,
-                        'pin_image' => $item->pinImage ? new IconResource($item->pinImage) : null,
-                        'trophy_image' => $item->trophyImage ? new IconResource($item->trophyImage) : null,
-                        'intro_audio' => $item->introAudio ? new AudioResource($item->introAudio) : null,
-                        'background_audio' => $item->backgroundAudio ? new AudioResource($item->backgroundAudio) : null,
-                    ],
+                    'main_image' => $item->mainImage ? $item->mainImage->path : null,
+                    'image1' => $item->image1 ? $item->image1->path : null,
+                    'image2' => $item->image2 ? $item->image2->path : null,
+                    'image3' => $item->image3 ? $item->image3->path : null,
+                    'pin_image' => $item->pinImage ? $item->pinImage->path : null,
+
+                    'intro_audio' => $item->introAudio ? $item->introAudio->path : null,
+                    'background_audio' => $item->backgroundAudio ? $item->backgroundAudio->path : null,
 
                     'created_at' => $item->created_at->toDateTimeString(),
                     'updated_at' => $item->updated_at ? $item->updated_at->toDateTimeString() : null,
