f78cf8ee3d5d2279d3ee367a88088c85ff741ea9
dan1114ho <dan1114ho@gmail.com>
2018-08-22 17:46:18 -0400

    Fixed deivce factory bug

--- database/factories/DeviceFactory.php
+++ database/factories/DeviceFactory.php
@@ -3,12 +3,13 @@
 use Faker\Generator as Faker;
 use App\Os;
 use App\DeviceType;
+use Illuminate\Support\Arr;
 
 $factory->define(App\Device::class, function (Faker $faker) {
     return [
         'device_udid' => md5(uniqid()),
-        'os' => array_rand(Os::all()),
-        'type' => array_rand(DeviceType::all()),
+        'os' => Arr::random(Os::all()),
+        'type' => Arr::random(DeviceType::all()),
         'user_agent' => null,
     ];
 });
