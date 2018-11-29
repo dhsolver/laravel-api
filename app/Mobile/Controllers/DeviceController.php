c0174a85737b0830831b08cfd197026b5ffff40e
dan1114ho <dan1114ho@gmail.com>
2018-11-29 11:00:01 -0500

    Removed unneccessary methods from device controller

--- app/Mobile/Controllers/DeviceController.php
+++ app/Mobile/Controllers/DeviceController.php
@@ -3,36 +3,15 @@
 namespace App\Mobile\Controllers;
 
 use App\Device;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreDeviceRequest;
 
 class DeviceController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param StoreDeviceRequest $request
      * @return \Illuminate\Http\Response
      */
     public function store(StoreDeviceRequest $request)
@@ -48,50 +27,5 @@ class DeviceController extends Controller
         return response()->json([
             'device_id' => $device->id,
         ]);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Device  $device
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Device $device)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Device  $device
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Device $device)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Device  $device
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Device $device)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Device  $device
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Device $device)
-    {
-        //
     }
 }
