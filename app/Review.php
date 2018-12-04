e0359af0929a01525178dd33f44a1a4c6a8f71c0
dan1114ho <dan1114ho@gmail.com>
2018-12-04 07:31:08 -0500

    Removed soft deletes from tour reviews

--- app/Review.php
+++ app/Review.php
@@ -3,12 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Review extends Model
 {
-    use SoftDeletes;
-
     /**
      * The attributes that aren't mass assignable.
      *
