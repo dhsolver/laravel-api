42db79336657c202f95f7e6b482e4a6b8dbbfc9a
dan1114ho <dan1114ho@gmail.com>
2019-01-15 08:31:54 -0500

    Add tour_limit to user factories so tests can pass

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -8,5 +8,6 @@ $factory->define(App\Admin::class, function (Faker $faker) {
         'email' => $faker->unique()->companyEmail,
         'password' => bcrypt('qweqwe'),
         'remember_token' => str_random(10),
+        'tour_limit' => 999,
     ];
 });
